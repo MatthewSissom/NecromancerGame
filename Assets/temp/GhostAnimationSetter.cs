8644a857c0616e9e863f3799fec4866162293fc8
MatthewSissom <matthewesissom@gmail.com>
2021-01-17 14:58:47 -0500

    added promo image scene

---
+++ Assets/temp/GhostAnimationSetter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class GhostAnimationSetter : MonoBehaviour
+{
+    Animator animator;
+
+    public float percentage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doOnUpdate();
+    }
+
+    private void doOnUpdate()
+    {
+            animator.Play("New State", 0, (percentage/100)%1);
+            animator.speed = 0;
+        
+        animator.Update(0);
+    }
+}
