02ff19f9b6cca3b25dd612c38b8c7a3ecac3092f
docmistersir <wsb6157@rit.edu>
2021-02-24 17:57:51 -0500

    commented assignment checker for clarity

--- Assets/AssignmentData/AssignmentChecker.cs
+++ Assets/AssignmentData/AssignmentChecker.cs
@@ -4,33 +4,47 @@ using UnityEngine;
 
 public class AssignmentChecker : MonoBehaviour
 {
-    private bool GetTransformData(Transform node)
+    // Loops through the table connection areas on a gameObject and returns true/false if they have bones
+    private bool GetBoneData(Transform node)
     {
+        // Get list of bones from a table connection area
         List<Bone> bones = GetComponent<TableConnectionArea>().GetAllBones();
+
+        // set hasBones to true/false if there are bones in the table connection area
         bool hasBones = bones != null && bones.Count > 0;
-        foreach (Transform child in gameObject.transform)
+
+        // loop through children of the parent node and get bone data from them
+        foreach (Transform child in node.transform)
         {
-            hasBones = GetTransformData(child) || hasBones;
+            hasBones = GetBoneData(child) || hasBones;
         }
 
+        // Display the bone in colsode log for testing purposes
         foreach (Bone bone in bones)
         {
             Debug.Log("Found Bone - " + bone);
         }
 
+        // return hasBones for recursive search purposes
         return hasBones;
     }
 
+    // Depth First search of a parent node
     public void DFSearch(Transform parent)
     {
+        // Check if there is table connection area component 
+        if (parent.GetComponent<TableConnectionArea>() != null)
+        {
+            // Get bone data if there is a table connection area
+            GetBoneData(parent);
+        }
+
+        // Loop trhough each child of the parent node
         foreach (Transform child in parent)
         {
+            // If a child has a child itself, search more recursively
             if (child.childCount > 0)
             {
-                if (child.GetComponent<TableConnectionArea>() != null)
-                {
-                    GetTransformData(child);
-                }
                 DFSearch(child);
             }
             else 
